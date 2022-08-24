463a181cf7fee475a2392da923f48b4271cbd93b
phnii <sudiryaya@gmail.com>
2022-08-24 12:02:30 +0900

    Bookモデル作成

---
+++ models/Book.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const BookSchema = new Schema({
+  day: {
+    type: Date,
+    required: [true, "日付を指定してください"]
+  },
+  startIndex: {
+    type: Number,
+    required: [true, "開始コマを指定してください"]
+  },
+  status: {
+    type: String,
+    enum: ["new", "determined", "done"],
+    default: "new"
+  },
+  menu: {
+    type: Schema.Types.ObjectId,
+    ref: "Menu",
+    required: [true, "メニューを選択してください"]
+  },
+  staff: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "スタッフを選択してください"]
+  }
+});
+
+module.exports = mongoose.model("Book", BookSchema);
